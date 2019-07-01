136e5496c27c9b8bc38e9957c04cd13dd5e6323f
DominikPott <d_tronic_p@gmx.de>
2019-07-01 22:21:47 +0200

    added control functin to add new files and reuse existing tags

--- tagging/control.py
+++ tagging/control.py
@@ -1,34 +1,45 @@
 import random
+import logging
 
 from tagging import Session
 from tagging.model import Item, Tag
 import tagging.filehash
 
+
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger('tagging')
+
 tag_names = ["snow", "fire", "lightning", "water", "simulation", "static"]
 
 
-def insert(filepath, tags_):
+def insert(filepath, tags_, session):
     tag_names_ = set(tags_)
-    all_tags = list_tags()
+    log.debug('Tag: %s' % tag_names_)
+    all_tags = list_tags(session=session)
+    log.debug(all_tags)
+    for t in all_tags:
+        log.debug("Existing Tag %s" % t.name)
     existing_tags = []
     for tag_name in tag_names_:
         for tag in all_tags:
-            if tag.name == tag_names:
+            log.debug('Existing tag: %s' % tag.name)
+            log.debug('Given Tag: %s' % tag_name)
+            if tag.name == tag_name:
                 existing_tags.append(tag)
                 break
         else:
             existing_tags.append(Tag(name=tag_name))
     hash_ = tagging.filehash.hash_from_file(filepath)
-    item_ = Item(name=filepath, hash=hash_)
-    session = Session()
+    item_ = Item(name=filepath, hash=hash_, tags=existing_tags)
+
     session.add(item_)
     session.commit()
+    session.close()
 
 
-def list_tags(file=None):
-    session = Session()
+def list_tags(session, file=None):
     if file:
-        return session.query(Item).one()
+        return session.query(Item).filter(Item.name == file).one()
     else:
         return session.query(Tag).all()
 
@@ -43,17 +54,15 @@ if __name__ == "__main__":
             filepath = os.path.join(path, file)
             yield (filepath)
 
-    """
+    session = Session()
     for filepath in test_data():
         tags = random.sample(tag_names, 2)
-        insert(filepath, tags)
-    """
+        insert(filepath, tags, session)
 
-    session = Session()
     items = session.query(Item).all()
     for item in items:
         tags = [tag.name for tag in item.tags]
-        print("%s: %s"%(item.name, tags))
+        print("%s: %s : %s"%(item.name, tags, item.hash))
 
     print("----------------")
 
@@ -63,4 +72,9 @@ if __name__ == "__main__":
         print("%s: %s"%(item.name, tags))
 
     print("All tags: %s" % ([tag.name for tag in session.query(Tag).order_by(Tag.name)]))
-    """
+
+    fire_tag = session.query(Tag).filter(Tag.name=="fire").all()
+    for f_tag in fire_tag:
+        print(f_tag.name)
+        for i in f_tag.items:
+            print(i.name)
